89700f95276986361515c78a9f009a9b1effd1ae
Vinícius Chagas <vinimaster@gmail.com>
2013-09-19 17:00:56 -0300

    Fixing lib name

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-alf',
+    name='djalf',
     version='0.1',
     description="OAuth Client",
     long_description='Django cache layer to alf, a OAuth Client based on requests.Session with seamless support for Client Credentials Flow',
     keywords='oauth client client_credentials requests django',
     author=u'Globo.com',
     author_email='j3@corp.globo.com',
-    url='https://github.com/globocom/django-alf',
+    url='https://github.com/globocom/djalf',
     license='Proprietary',
     classifiers=['Intended Audience :: Developers'],
     packages=find_packages(
