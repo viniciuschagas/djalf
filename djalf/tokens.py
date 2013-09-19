4c90aad93ea3133fd9c41a46f7fa14e50d75dcea
Vinícius Chagas <vinimaster@gmail.com>
2013-09-19 17:09:25 -0300

    Implementing TokenDjango

--- djalf/tokens.py
+++ djalf/tokens.py
@@ -1,20 +1,24 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
+
+from django.core.cache import cache
 from alf.tokens import Token
 
 
 class TokenDjango(Token):
-    def __init__(self, access_token='', expires_in=0):
-        self.access_token = access_token
 
+    def __init__(self, access_token='', expires_in=0):
         super(TokenDjango, self).__init__(access_token=access_token, expires_in=expires_in)
+
+        self.access_token = access_token
 
     @property
     def access_token(self):
-        pass
+        return cache.get('account_access_token')
 
     @access_token.setter
     def access_token(self, access_token):
-        pass
+        cache.set('account_access_token', access_token, self.expires_in)
 
     def is_valid(self):
-        pass
+        return cache.get('account_access_token') and (self._expires_on >= datetime.now())
