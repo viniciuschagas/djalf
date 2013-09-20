d26d5519c59a0d1b483d6eb2a1acd4daaea01691
Vinícius Chagas <vinimaster@gmail.com>
2013-09-20 16:53:51 -0300

    Update to use the new concept of token data storage adopted by alf

--- djalf/managers.py
+++ djalf/managers.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+from django.core.cache import cache
 from alf.managers import TokenManager
-from djalf.tokens import TokenDjango
 
 
 class TokenManagerDjango(TokenManager):
 
-    def __init__(self, *args, **kwargs):
-        super(TokenManagerDjango, self).__init__(*args, **kwargs)
+    def _get_from_cache(self):
+        return cache.get(self._token_endpoint)
 
-        self._token = TokenDjango()
+    def _get_token_data(self):
+        token_data = self._get_from_cache()
+        if token_data:
+            return token_data
+
+        return super(TokenManagerDjango, self)._get_token_data()
+
+    def _request_token(self):
+        token_data = super(TokenManagerDjango, self)._request_token()
+        cache.set(self._token_endpoint, token_data, token_data.get('expires_in', 0))
+        return token_data
--- tests/managers/test_django_manager.py
+++ tests/managers/test_django_manager.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
+from mock import patch
 from unittest import TestCase
 
-from djalf.tokens import TokenDjango
 from djalf.managers import TokenManagerDjango
 
 
@@ -13,5 +13,36 @@ class TestTokenManagerDjango(TestCase):
             'http://endpoint/token', 'client_id', 'client_secret'
         )
 
-    def test_should_use_token_django(self):
-        self.assertTrue(isinstance(self.manager._token, TokenDjango))
+    @patch('djalf.managers.cache.get')
+    def test_get_from_cache_should_returns_data_stored_in_cache(self, cache_get):
+        self.manager._get_from_cache()
+
+        cache_get.assert_called_once_with(self.manager._token_endpoint)
+
+    @patch('alf.managers.TokenManager._request_token')
+    @patch('djalf.managers.cache.set')
+    def test_request_token_should_store_token_data_in_cache(self, cache_set, _request_token):
+        token_data = {'access_token': 'access_token', 'expires_in': 10}
+
+        _request_token.return_value = token_data
+
+        self.manager._request_token()
+
+        cache_set.assert_called_once_with(self.manager._token_endpoint, token_data, 10)
+
+    @patch('djalf.managers.TokenManagerDjango._get_from_cache')
+    def test_get_token_data_should_verify_if_do_have_token_data_in_cache(self, _get_from_cache):
+        _get_from_cache.return_value = '{}'
+
+        self.manager._get_token_data()
+
+        _get_from_cache.assert_called_once()
+
+    @patch('alf.managers.TokenManager._get_token_data')
+    @patch('djalf.managers.TokenManagerDjango._get_from_cache')
+    def test_get_token_data_should_call_super_class_get_token_if_does_not_have_token_data_in_cache(self, _get_from_cache, original_get_token_data):
+        _get_from_cache.return_value = ''
+
+        self.manager._get_token_data()
+
+        original_get_token_data.assert_called_once()
